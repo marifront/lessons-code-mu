dbc994b0cc2f93b5359122c42aaaffc8d9ab9556
Maria <volkovaweb@yandex.ru>
2022-07-05 23:16:27 +0600

    add lesson 148.js

---
+++ lesson-148.js
@@ -0,0 +1,29 @@
+// Задача 1
+// let myArr = [1, 2, 3, 4, 5];
+
+// function func(arr){
+// 	console.log(arr.shift());
+	
+// 	if (arr.length !==0){
+// 		func(arr); 
+// 	}
+// }
+
+// func(myArr);
+
+//Задача 2
+
+// let arr = [1, 2, 3, 4, 5];
+
+// function getSum(arr) {
+// 	let sum = arr.shift() **2 ;
+//   console.log(sum)
+	
+// 	if (arr.length !== 0) {
+// 		sum += getSum(arr);
+// 	}
+	
+// 	return sum;
+// }
+
+// console.log(getSum(arr));
